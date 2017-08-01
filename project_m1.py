8847effcaac5e0e3841e77e793e9428d62a3f2fe
mpigelati <saimohansai@gmail.com>
2017-08-01 16:26:17 +0530

    deleting some files

--- project_m1.py
+++ project_m1.py
@@ -1,37 +1,24 @@
-#print ("hellow world")
 import subprocess
 import os
-#subprocess.call('git status > git_info.txt',shell=True)
+subprocess.call('git status > git_info.txt',shell=True)
 
-	
-#os.system("git status > git_info.txt")
-
-#print(subprocess(["cat",""]))
-def Fun_Remove(My_list):
-	length=len(My_list)
-	
-	for i in (1, length):
-		My_list[i-1]=My_list[i-1].lstrip(" ").lstrip("\t").rstrip("\n")
-	return My_list
-
+def Fun_Remove_1(temp):
+	temp1 = temp.lstrip(" ").lstrip("\t").rstrip("\n")
+	return temp1
 def Fun_print_file(Fd):
     Zip=[]
     P_list = Fd.readlines()
     count=len(P_list)
     for i, line in enumerate(P_list):
 	line = line.rstrip()
-	#print i,  line
         if(line !='\n'):
-            #print(line)
             if "modified:" in line :
                 temp = "".join(line.strip( ).split(':')[1])
-                Zip.append(temp)
+                temp = Fun_Remove_1(temp)
+		Zip.append(temp)
             elif "Untracked" in line:
-		#print "got untracked files"
 		for j in range(i+3, count):
-			#if (len(P_list[j]) > 0):
 			if (P_list[j].isspace() == False):
-				#print "===", P_list[j]
 				Zip.append(P_list[j])	
 			else:
 				return Zip
@@ -45,17 +32,12 @@ def Fun_open_File(Filename,mode):
         print("unable to open file")
         exit(1)
     else:
-        #print("file opened success fully")
         return fd
 
 zip=[]
 Fd = Fun_open_File("git_info.txt","r")
-#print(fd)
 zip = Fun_print_file(Fd)
-zip=Fun_Remove(zip)
-#print ("Zip",zip)
 string= "tar -rvf patch.tar " +" ".join(zip)
-print ("string",string)
 
 subprocess.call(string,shell=True)
 
